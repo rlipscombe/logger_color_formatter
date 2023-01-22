8d4b68be41c48cd6b1f1ed9ddc9f5e59f85938d8
Roger Lipscombe <roger@differentpla.net>
2023-01-22 10:01:40 +0000

    Allow overriding colours in config

--- src/logger_color_formatter.erl
+++ src/logger_color_formatter.erl
@@ -2,30 +2,42 @@
 
 -export([config_check/1, format/2]).
 
+-define(DEFAULT_FORMATTER, logger_formatter).
+
 config_check(Config) ->
-    logger_formatter:check_config(update_config(Config, debug)).
+    ?DEFAULT_FORMATTER:check_config(update_config(Config, debug)).
 
 format(LogEvent = #{level := Level}, Config) ->
     Config2 = update_config(Config, Level),
-    logger_formatter:format(LogEvent, Config2).
+    ?DEFAULT_FORMATTER:format(LogEvent, Config2).
 
+update_config(Config = #{template := Template, colors := Colors},
+              Level) ->
+    Config#{template =>
+                update_template(Template, maps:merge(default_colors(), Colors), Level)};
 update_config(Config = #{template := Template}, Level) ->
-    Config#{template => update_template(Template, Level)};
+    Config#{template => update_template(Template, default_colors(), Level)};
 update_config(Config, _Level) ->
     Config.
 
-update_template(Template, Level) ->
-    lists:map(fun
-            (color) -> default_color(Level);
-            (reset) -> "\e[0m";
-            (Item) -> Item
-        end, Template).
+update_template(Template, Colors, Level) ->
+    lists:map(fun (color) ->
+                      maps:get(Level, Colors);
+                  (reset) ->
+                      maps:get(reset, Colors);
+                  (Item) ->
+                      Item
+              end,
+              Template).
 
-default_color(debug) -> "\e[0;38m";
-default_color(info) -> "\e[1;37m";
-default_color(notice) -> "\e[1;36m";
-default_color(warning) -> "\e[1;33m";
-default_color(error) -> "\e[1;31m";
-default_color(critical) -> "\e[1;35m";
-default_color(alert) -> "\e[1;44m";
-default_color(emergency) -> "\e[1;41m".
+default_colors() ->
+    #{debug => "\e[0;38m",
+      info => "\e[1;37m",
+      notice => "\e[1;36m",
+      warning => "\e[1;33m",
+      error => "\e[1;31m",
+      critical => "\e[1;35m",
+      alert => "\e[1;44m",
+      emergency => "\e[1;41m",
+      % Not a colour, but this'll do.
+      reset => "\e0m"}.
