69dd62f174e59fab483bfb460937c485eb7d1ae6
Roger Lipscombe <roger@differentpla.net>
2023-01-22 10:02:13 +0000

    Support NO_COLOR; see https://no-color.org/

--- src/logger_color_formatter.erl
+++ src/logger_color_formatter.erl
@@ -5,19 +5,20 @@
 -define(DEFAULT_FORMATTER, logger_formatter).
 
 config_check(Config) ->
-    ?DEFAULT_FORMATTER:check_config(update_config(Config, debug)).
+    ?DEFAULT_FORMATTER:check_config(update_config(os:getenv("NO_COLOR"), Config, debug)).
 
 format(LogEvent = #{level := Level}, Config) ->
-    Config2 = update_config(Config, Level),
+    Config2 = update_config(os:getenv("NO_COLOR"), Config, Level),
     ?DEFAULT_FORMATTER:format(LogEvent, Config2).
 
-update_config(Config = #{template := Template, colors := Colors},
+update_config(_NoColor = false,
+              Config = #{template := Template, colors := Colors},
               Level) ->
     Config#{template =>
                 update_template(Template, maps:merge(default_colors(), Colors), Level)};
-update_config(Config = #{template := Template}, Level) ->
+update_config(_NoColor = false, Config = #{template := Template}, Level) ->
     Config#{template => update_template(Template, default_colors(), Level)};
-update_config(Config, _Level) ->
+update_config(_NoColor, Config, _Level) ->
     Config.
 
 update_template(Template, Colors, Level) ->
